de6699a88af1b0643348aa0d61921c438e3d0aff
TeddyMedjio <nganmedjiojeanclaude@gmail.com>
2025-06-23 22:25:42 +0100

    open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      {/* <!-- HTML Meta Tags --> */}
+      {/* <!-- Facebook Meta Tags --> */}
+      <meta property="og:url" content="https://dalia-rho.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Dalia Environnement" />
+      <meta
+        property="og:description"
+        content="Des solutions papier haut de gamme pour vos besoins."
+      />
+      <meta
+        property="og:image"
+        content="https://opengraph.b-cdn.net/production/images/9af1431b-847a-4d2a-bfc2-cdcc5de44e91.png?token=mrQkfSeC5wdQcNPW-HqrUIaWhtOGs0cq4T2xQm822Ik&height=643&width=1200&expires=33286713745"
+      />
+
+      {/* <!-- Twitter Meta Tags --> */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="dalia-rho.vercel.app" />
+      <meta property="twitter:url" content="https://dalia-rho.vercel.app/" />
+      <meta name="twitter:title" content="Dalia Environnement" />
+      <meta
+        name="twitter:description"
+        content="Des solutions papier haut de gamme pour vos besoins."
+      />
+      <meta
+        name="twitter:image"
+        content="https://opengraph.b-cdn.net/production/images/9af1431b-847a-4d2a-bfc2-cdcc5de44e91.png?token=mrQkfSeC5wdQcNPW-HqrUIaWhtOGs0cq4T2xQm822Ik&height=643&width=1200&expires=33286713745"
+      />
+
       <body className={`${geist.variable} antialiased`}>
         <Navbar />
         {children}
