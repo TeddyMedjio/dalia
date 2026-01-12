6ddb35428764c7c826c44273b5c0cf5842381c70
TeddyMedjio <nganmedjiojeanclaude@gmail.com>
2026-01-12 14:42:41 +0100

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
         <div className="grid place-items-center py-20 space-y-10">
           <h1 className=" text-center text-vert-fonce text-[34px] leading-10 md:text-5xl font-medium md:leading-14 max-w-4xl">
             Oupps!! <br />
-            La page n&apos;existe plus ou a été déplacé.
+            La page n&apos;existe plus ou a été déplacé
           </h1>
 
           <Link
